36e9346725e0380703bf2191232bb451d4419624
MrazTevin <millatevin@gmail.com>
2023-01-04 17:22:30 +0300

    m: include special characters in password

--- PassMain.java
+++ PassMain.java
@@ -11,11 +11,12 @@ public class PassMain {
         String lower_cases = "qwertyuiopasdfghjklzxcvbnm";
         String upper_cases = "QWERTYUIOPASDFGHJKLZXCVBNM";
         String numbers = "0123456789";
+        String special_characters = "!@#$%^&*()";
 
         String password = "";
 
         for(int i=0; i<digit; i++) {
-            int rand = (int)(3 * Math.random());
+            int rand = (int)(4 * Math.random());
 
             switch(rand){
                 case 0:
@@ -30,8 +31,11 @@ public class PassMain {
                     rand = (int)(upper_cases.length() * Math.random());
                     password += String.valueOf(upper_cases.charAt(rand));
                     break;
-        }
-
+                case 3:
+                    rand = (int)(special_characters.length() * Math.random());
+                    password += String.valueOf(special_characters.charAt(rand));
+                    break;
+            }
     }
 
         System.out.println("Your password is :");
